83ef96e762d7bc6f1301e71d4fa997e44af41c95
DANIEL TAPP <danieltapp@gmail.com>
2024-12-07 20:20:08 -0500

    initial project setup...

---
+++ README.md
@@ -0,0 +1,8 @@
+# DT FYI 
+
+Who decided on a Friday evening, "hey I should spin up a Svelte project where I intro myself and render my Bluesky feed..."... I did. Why am I using a framework at all for this? Yeah. These are great questions...
+
+## Goals 🎯
+- Render Bluesky posts 🌌
+- Provide contact info 📞
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Header from '$lib/Header.svelte';
+  import Intro from '$lib/Intro.svelte';
+  import SocialLinks from '$lib/SocialLinks.svelte';
+  import Canvas from '$lib/Canvas.svelte';
+  import "../app.css";
+  // Uncomment the line below if you plan to include BlueskyFeed later
+  // import BlueskyFeed from '$lib/BlueskyFeed.svelte';
+</script>
+
+<Canvas />
+<main>
+  <Header />
+  <SocialLinks />
+  <Intro />
+  <!-- Uncomment the line below to include BlueskyFeed -->
+  <!-- <BlueskyFeed /> -->
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 0 1rem;
+  }
+</style>
