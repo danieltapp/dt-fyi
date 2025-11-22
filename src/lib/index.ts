7096b862211b74fb42ead82abe3c3c4fb7893973
Daniel Tapp <31320993+danieltapp@users.noreply.github.com>
2025-11-22 16:19:29 -0500

    Refactor to use vanilla HTML, CSS, & JS 🎨 (#4)
    * Refactor to use vanilla html, css, & js.
    
    * Add vercel.json to specify output directory

--- src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Header } from "./Header.svelte";
-export { default as Canvas } from "./Canvas.svelte";
-export { default as Intro } from "./Intro.svelte";
-export { default as BlueskyFeed } from "./BlueskyFeed.svelte";
-export { default as BlueskyPost } from "./BlueskyPost.svelte";
-export { default as SocialLinks } from "./SocialLinks.svelte";
